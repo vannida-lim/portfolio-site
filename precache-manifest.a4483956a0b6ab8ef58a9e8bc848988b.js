b7e51e08820d6453394cdd910cdf13bb173ee636
Vannida Lim <vannida.lim@gmail.com>
2020-05-25 23:30:12 -0400

    Updates

---
+++ precache-manifest.a4483956a0b6ab8ef58a9e8bc848988b.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b44c07600156af8aabbc1606df78ecae",
+    "url": "/index.html"
+  },
+  {
+    "revision": "452f4a0bc0f7da9bd1c5",
+    "url": "/static/css/main.f350f449.chunk.css"
+  },
+  {
+    "revision": "ce22792b40605775238b",
+    "url": "/static/js/2.2e906c53.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/static/js/2.2e906c53.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "452f4a0bc0f7da9bd1c5",
+    "url": "/static/js/main.40cf4159.chunk.js"
+  },
+  {
+    "revision": "1d174dd0b0ae47ee00c0",
+    "url": "/static/js/runtime-main.60707d35.js"
+  }
+]);
