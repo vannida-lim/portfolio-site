a69c38fdfeb3f67c9b10f28822379d67e33eed94
Vannida Lim <vannida.lim@gmail.com>
2020-06-15 13:26:41 -0400

    Updates

---
+++ precache-manifest.408f1167d73f38f6b05c9d10756beebf.js
@@ -0,0 +1,42 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "7013b641df6b50d9115dcc43ee2f17b4",
+    "url": "/index.html"
+  },
+  {
+    "revision": "75118a533dd85c4d00e1",
+    "url": "/static/css/main.af802bef.chunk.css"
+  },
+  {
+    "revision": "b7c263493337f09a32bf",
+    "url": "/static/js/2.4aa37df1.chunk.js"
+  },
+  {
+    "revision": "4ba1a1c11c57ed15aa68008707a3d6b4",
+    "url": "/static/js/2.4aa37df1.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "75118a533dd85c4d00e1",
+    "url": "/static/js/main.4d0d21b8.chunk.js"
+  },
+  {
+    "revision": "1d174dd0b0ae47ee00c0",
+    "url": "/static/js/runtime-main.60707d35.js"
+  },
+  {
+    "revision": "b24679f5bfdcbb7ed8ce86bb844ada56",
+    "url": "/static/media/demo1.fd917dfc.png"
+  },
+  {
+    "revision": "bbf6b34b3f6e198c4a89cdcf8b5a8165",
+    "url": "/static/media/demo2.f03e80d0.png"
+  },
+  {
+    "revision": "3d2d3d4eaea5e6c8a0f56e4bd9499d50",
+    "url": "/static/media/demo3.3a85daf4.png"
+  },
+  {
+    "revision": "a04e680d586797a1332687ca699f61fc",
+    "url": "/static/media/myProfile.1ce5abe0.png"
+  }
+]);
